bfc04525c2ab5f0d8f9a41c6573c36bff8ca6378
Alessio Manieri <mani.alexander03@gmail.com>
2021-05-26 07:27:49 +0200

    Aggiungere i file di progetto.

---
+++ WpfSmuseum/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSmuseum
+{
+    /// <summary>
+    /// Logica di interazione per App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var splashScreen = new SplashScreen();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+            Task.Factory.StartNew(() =>
+            {
+
+                for (int i = 1; i <= 100; i++)
+                {
+                    System.Threading.Thread.Sleep(50);
+
+                }
+                this.Dispatcher.Invoke(() =>
+                {
+
+                    var mainWindow = new MainWindow();
+                    this.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    splashScreen.Close();
+                });
+            });
+        }
+    }
+}
